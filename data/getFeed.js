2f707aeffd1e9d9107c42c56c075a44a3152e517
Andrew Dushane <andrewdushane@gmail.com>
2020-05-25 18:15:05 -0400

    Basic SSR setup

---
+++ data/getFeed.js
@@ -0,0 +1,29 @@
+import feedRead from "feed-read";
+import Parser from "rss-parser";
+import feed from "./feed.json";
+
+const parser = new Parser();
+
+export const getFeed = async () => {
+  const composedFeed = {};
+  await Promise.all(
+    feed.map(async (source) => {
+      try {
+        const sourceFeed = await parser.parseURL(source.rss_url);
+        const articles = sourceFeed?.items;
+        if (!articles) {
+          return;
+        }
+        composedFeed[source.id] = {
+          ...source,
+          articles: sourceFeed.items,
+        };
+        return;
+      } catch (e) {
+        // skip any sources that can't be parsed
+        return;
+      }
+    })
+  );
+  return composedFeed;
+};
